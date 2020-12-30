8de67936107b6a268aa1b16eb10e8293c9b63d7a
“Brett <ddispp@gmail.com>
2020-12-30 01:08:29 -0500

    clear old amplify

--- serve.sh
+++ serve.sh
@@ -1,3 +1,7 @@
-find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
+
+npm install -g @angular/cli
+npm install -g @aws-amplify/cli
 npm i
+amplify pull --appId d2qedav01jvzfi --envName master
 ng serve
+find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
