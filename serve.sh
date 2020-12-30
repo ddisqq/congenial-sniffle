0a43ab0fb7153a4a5427c386d65a5e28c82986ac
“Brett <ddispp@gmail.com>
2020-12-30 01:53:27 -0500

    amplify add function

--- serve.sh
+++ serve.sh
@@ -2,5 +2,6 @@
 npm install -g @angular/cli
 npm install -g @aws-amplify/cli
 npm i
+amplify pull --appId d2qedav01jvzfi --envName staging
 ng serve
 find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
