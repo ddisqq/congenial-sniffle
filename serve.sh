c18574ee76a98a0bb28e9e0ab811fc0df647d132
“Brett <ddispp@gmail.com>
2020-12-30 01:32:13 -0500

    dev branch

--- serve.sh
+++ serve.sh
@@ -2,6 +2,5 @@
 npm install -g @angular/cli
 npm install -g @aws-amplify/cli
 npm i
-amplify pull --appId d2qedav01jvzfi --envName master
 ng serve
 find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
