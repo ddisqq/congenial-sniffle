1a2cec04dce419352cde72bb570f06e6c80501d0
“Brett <ddispp@gmail.com>
2020-12-30 02:04:30 -0500

    update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,7 +1,9 @@
 
-npm install -g @angular/cli
-npm install -g @aws-amplify/cli
-npm i
+
+yarn
+yarn add @angular/cli
+yarn add @aws-amplify/cli
+yarn add aws-amplify @aws-amplify/ui-angular
 amplify pull --appId d2qedav01jvzfi --envName staging
 ng serve
 open https://dev.d328v7rxk7g2vo.amplifyapp.com
@@ -10,3 +12,7 @@ open https://dev.my3dbank.com
 open https://my3dbank.com
 open https://www.my3dbank.com
 find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
+
+npm install -g @angular/cli
+npm install -g @aws-amplify/cli
+npm i
