22aa94985c702df8bbc0589f05476d5b30e4201f
“Brett <ddispp@gmail.com>
2020-12-30 02:00:06 -0500

    amplify push and publish

--- serve.sh
+++ serve.sh
@@ -4,4 +4,9 @@ npm install -g @aws-amplify/cli
 npm i
 amplify pull --appId d2qedav01jvzfi --envName staging
 ng serve
+open https://dev.d328v7rxk7g2vo.amplifyapp.com
+open https://localhost:4200
+open https://dev.my3dbank.com
+open https://my3dbank.com
+open https://www.my3dbank.com
 find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
