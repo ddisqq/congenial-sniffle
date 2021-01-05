cb5f2a2ee730b4bee8b54918ad80c2d6c838a962
“Brett <ddispp@gmail.com>
2021-01-04 22:23:46 -0500

    start headers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token',
+  }),
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,7 +19,29 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
+  //   CB-ACCESS-KEY The api key as a string.
+  // CB-ACCESS-SIGN The base64-encoded signature (see Signing a Message).
+  // CB-ACCESS-TIMESTAMP A timestamp for your request.
+  // CB-ACCESS-PASSPHRASE The passphrase you specified when creating the API key.
+
   ngOnInit() {
+    //SET Headers
+    // httpOptions.headers = httpOptions.headers.set(
+    //   'Authorization',
+    //   'my-new-auth-token'
+    // );
+    const headerDict = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    };
+
+    const requestOptions = {
+      headers: new Headers(headerDict),
+    };
+
+    var apiUrl = 'https://api-public.sandbox.pro.coinbase.com';
+
     // Simple GET request with response type <any>
     this.http
       .get<any>('https://api.npms.io/v2/search?q=scope:angular')
@@ -19,7 +49,6 @@ export class AppComponent implements OnInit {
         this.totalAngularPackages = data.total;
       });
 
-    var apiUrl = 'https://api-public.sandbox.pro.coinbase.com';
     this.http.get<any>(apiUrl).subscribe((data) => {
       this.coinBase = data.total;
     });
