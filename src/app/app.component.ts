94ef8aea8d78b85a5f1a1f5017ab8c91f23f97c6
“Brett <ddispp@gmail.com>
2021-01-04 22:15:16 -0500

    GET and index cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';@Component({
+import { HttpClient } from '@angular/common/http';
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   totalAngularPackages;
+  coinBase;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {      
-      // Simple GET request with response type <any>
-      this.http.get<any>('https://api.npms.io/v2/search?q=scope:angular').subscribe(data => {
-          this.totalAngularPackages = data.total;
-      })
+  ngOnInit() {
+    // Simple GET request with response type <any>
+    this.http
+      .get<any>('https://api.npms.io/v2/search?q=scope:angular')
+      .subscribe((data) => {
+        this.totalAngularPackages = data.total;
+      });
+
+    var apiUrl = 'https://api-public.sandbox.pro.coinbase.com';
+    this.http.get<any>(apiUrl).subscribe((data) => {
+      this.coinBase = data.total;
+    });
+
+    console.log(this.coinBase);
   }
 }
