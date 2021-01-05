4d0721f487c0c16f16f33189a170b188bc1a5f45
“Brett <ddispp@gmail.com>
2021-01-04 23:05:11 -0500

    signing work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    Authorization: 'my-auth-token',
-  }),
-};
 
 @Component({
   selector: 'app-root',
@@ -25,34 +19,48 @@ export class AppComponent implements OnInit {
   // CB-ACCESS-PASSPHRASE The passphrase you specified when creating the API key.
 
   ngOnInit() {
-    //SET Headers
-    // httpOptions.headers = httpOptions.headers.set(
-    //   'Authorization',
-    //   'my-new-auth-token'
-    // );
-    const headerDict = {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    };
+    var secret = 'PYPd1Hv4J6/7x...';
+    var timestamp = Date.now() / 1000;
+    var requestPath = '/orders';
+    var body = JSON.stringify({
+      price: '1.0',
+      size: '1.0',
+      side: 'buy',
+      product_id: 'BTC-USD',
+    });
 
-    const requestOptions = {
-      headers: new Headers(headerDict),
-    };
+    // var holdHmac = hmac.update(what).digest('base64');
+    var timestamp = Date.now() / 1000;
+    var method = 'POST';
+    var what = timestamp + method + requestPath + body;
+    // var key = new Buffer('71disque', 'base64');
 
+    // var hmac = require('crypto')
+    //   .createHmac('SHA256', key)
+    //   .update('MESSAGE')
+    //   .digest('base64');
+    // const headerDict = {
+    //   'Content-Type': 'application/json',
+    //   Accept: 'application/json',
+    //   'Access-Control-Allow-Headers': 'Content-Type',
+    //   'CB-ACCESS-KEY':
+    //     'Dt3u26EB9u8NyWMJ2UF3DNn1oTCDhf2OuOhaXhZJyr/ZW4JNVPqoaTXgarSEa3L1dTn3+iinh1hJHBwsT7vIDQ==',
+    //   'CB-ACCESS-SIGN': holdHmac.toString(),
+    //   'CB-ACCESS-TIMESTAMP': timestamp.toString(),
+    //   'CB-ACCESS-PASSPHRASE': '71disque',
+    // };
+    // const requestOptions = {
+    //   headers: new Headers(headerDict),
+    // };
     var apiUrl = 'https://api-public.sandbox.pro.coinbase.com';
-
-    // Simple GET request with response type <any>
     this.http
       .get<any>('https://api.npms.io/v2/search?q=scope:angular')
       .subscribe((data) => {
         this.totalAngularPackages = data.total;
       });
-
     this.http.get<any>(apiUrl).subscribe((data) => {
       this.coinBase = data.total;
     });
-
     console.log(this.coinBase);
   }
 }
